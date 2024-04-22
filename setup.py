d3edbf22016f5d4a32ce8049885612f3b690cc8a
Yohan <20425672+whymath@users.noreply.github.com>
2024-04-22 16:14:16 -0400

    Updating chex version to fix AttributeError module jax.random has no attribute KeyArray

--- setup.py
+++ setup.py
@@ -106,7 +106,8 @@ setuptools.setup(
     python_requires='>=3.10',
     install_requires=[
         'absl-py',
-        'chex<0.1.81',  # Incompatible with tensorflow 2.13 (due to numpy req).
+        # 'chex<0.1.81',  # Incompatible with tensorflow 2.13 (due to numpy req).
+        'chex==0.1.86',
         'dm-env',
         'dmlab2d',
         'dm-tree',
